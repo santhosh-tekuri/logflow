57d1c8d05a5f90d6e7d6f367d95fb34b2d9a6db0
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2019-09-01 23:13:18 +0530

    script to publish docker images

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM golang:1.12.9 as gobuild
-WORKDIR /logflow
-COPY . .
-RUN CGO_ENABLED=0 go install -a
-
-FROM scratch
-COPY --from=gobuild /go/bin/logflow /
-ENTRYPOINT ["/logflow"]
-
---
+++ docker-publish.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+if [[ $# -ne 1 ]]; then
+    echo "Usage: docker-publish.sh TAG"
+    exit 1
+fi
+image=logflow/logflow:$1
+latest=logflow/logflow
+
+cat <<EOF > Dockerfile
+FROM scratch
+COPY logflow /
+ENTRYPOINT ["/logflow"]
+EOF
+trap "rm -f Dockerfile logflow" EXIT
+
+archs=(amd64 arm arm64 ppc64 ppc64le s390x)
+declare -a images
+for arch in "${archs[@]}"; do
+    echo bulding ${image}-${arch} ----------------------
+    rm -f logflow
+    docker run --rm -v "$PWD":/logflow -w /logflow -e GOARCH=${arch} -e CGO_ENABLED=0 golang:1.12.9 go build -a
+    file ./logflow
+    docker build -t ${image}-${arch} .
+    docker push ${image}-${arch}
+    images+=(${image}-${arch})
+done
+
+echo buildings manifests ----------------------
+docker manifest create --amend ${image} ${images[@]}
+docker manifest create --amend ${latest} ${images[@]}
+for arch in "${archs[@]}"; do
+  docker manifest annotate ${image} ${image}-${arch} --os linux --arch ${arch}
+  docker manifest annotate ${latest} ${image}-${arch} --os linux --arch ${arch}
+done
+docker manifest push ${image}
+docker manifest push ${latest}
